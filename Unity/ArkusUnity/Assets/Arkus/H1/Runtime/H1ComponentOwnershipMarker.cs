3c66ca6e691b9c09cef59caa0f9baa3ddd8246c6
Arkus0 <nildest@gmail.com>
2026-09-25 08:19:19 +0200

    h1-07: bind owned component overrides to exact object paths

--- Unity/ArkusUnity/Assets/Arkus/H1/Runtime/H1ComponentOwnershipMarker.cs
+++ Unity/ArkusUnity/Assets/Arkus/H1/Runtime/H1ComponentOwnershipMarker.cs
@@ -8,6 +8,8 @@ namespace Arkus.H1.Projection
         public const string SchemaId = "arkus.h1.component-ownership@1";
         public string schemaId = SchemaId;
         public bool rendererMaterial;
+        public string rendererRelativePath = "";
         public bool animatorClip;
+        public string animatorRelativePath = "";
     }
 }
