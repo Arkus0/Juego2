d6696fd6974a52476bff80ba56d5c6594b82776b
Arkus0 <nildest@gmail.com>
2026-09-25 23:21:30 +0200

    fix(h1-10): import world-state source contract

--- tools/Arkus.H1.UnityHost/H1ProjectCheckpointComposition.cs
+++ tools/Arkus.H1.UnityHost/H1ProjectCheckpointComposition.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Arkus.Game.Authoring;
 using Arkus.Game.World;
 using Arkus.Harness.Projection;
 using Arkus.Harness.Protocol;
