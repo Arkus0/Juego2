a549ced289684de813f26102740d2550a1149d8d
Arkus0 <nildest@gmail.com>
2026-09-25 07:51:29 +0200

    h1-07: mark explicitly owned component override fields

---
+++ Unity/ArkusUnity/Assets/Arkus/H1/Runtime/H1ComponentOwnershipMarker.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Arkus.H1.Projection
+{
+    [DisallowMultipleComponent]
+    public sealed class H1ComponentOwnershipMarker : MonoBehaviour
+    {
+        public const string SchemaId = "arkus.h1.component-ownership@1";
+        public string schemaId = SchemaId;
+        public bool rendererMaterial;
+        public bool animatorClip;
+    }
+}
